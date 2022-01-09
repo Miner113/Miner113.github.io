9dc7a567bbde56ca6580610198ceeb604244e6f1
Miner113 <89903767+Miner113@users.noreply.github.com>
2022-01-10 02:40:29 +0700

    1

---
+++ index.js
@@ -0,0 +1,4 @@
+import * as ccimp from './cryptoconditions-js/pkg/cryptoconditions.js';
+window.ccimp = ccimp;
+console.log(ccimp);
+import app from './src';
---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const explorerApiUrl = 'https://explorer.komodoplatform.com:10000/grms/api/';
+const coin = 'GRMS';
+const explorerUrl = 'http://explorer.komodoplatform.com:20000/tokens';
+const txBuilderApi = 'insight'; // default|insight
+const faucetURL = 'https://www.atomicexplorer.com/#/faucet/grms/';
+
+module.exports = {
+  explorerUrl,
+  explorerApiUrl,
+  coin,
+  txBuilderApi,
+  faucetURL,
+};
